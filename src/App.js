20718cb80303339efab931adc8e7e53e7679ebdd
Hyonsok <nahyonsok@gmail.com>
2019-06-30 11:11:39 -0700

    List Customers

--- src/App.js
+++ src/App.js
@@ -2,23 +2,54 @@ import React, {Component} from 'react';
 import Customer from './Components/Customer';
 import './App.css';
 
-const customer = {
+// by adding [], customers are in an array
+const customers = [
+{
+  'id' : 1,
+  'image' :  'https://placeimg.com/64/64/1', // place any random image site 64x64
   'name' : 'Hyonsok',
   'birthday' : '20190629',
   'gender' : 'Male',
   'job' : 'student'
+},
+{
+  'id' : 2,
+  'image' :  'https://placeimg.com/64/64/2', 
+  'name' : 'Alex ',
+  'birthday' : '20190630',
+  'gender' : 'Male',
+  'job' : 'student'
+},
+{
+  'id' : 3,
+  'image' :  'https://placeimg.com/64/64/3', // place any random image site 64x64
+  'name' : 'Claire',
+  'birthday' : '201900619',
+  'gender' : 'Female',
+  'job' : 'student'
 }
+]
 
+// identified primary key, customer.id
+// iterate the customers by using map
 class App extends Component{
   render() {
     return (
-      <Customer
-        name = {customer.name}
-        birthday = {customer.birthday}
-        gender = {customer.gender}
-        job = {customer.job}
-      />
-
+      <div>
+        {
+          customers.map(c => {
+            return (<Customer 
+              key = {c.id}
+              id = {c.id}
+              image = {c.image}
+              name = {c.name}
+              birthday = {c.birthday}
+              gender = {c.gender}
+              job = {c.job}
+            />);
+          })
+        }
+      </div>
       );
     }
   }
--- src/Components/Customer.js
+++ src/Components/Customer.js
@@ -5,12 +5,37 @@ class Customer extends React.Component {
     {
         return (
         <div>
-            <h2>{this.props.name}</h2>
+            <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
+            <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
+           
+        </div> 
+        )
+    }
+}
+
+// added if there is a lot of customer information
+// placed customer image
+// displayed customer name(id)
+class CustomerProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <img src = {this.props.image} alt="profile"/>
+                <h2> {this.props.name}({this.props.id})</h2> 
+            </div>
+        )
+    }
+}
+
+class CustomerInfo extends React.Component {
+    render() {
+        return (
+            <div>
             <p>{this.props.birthday}</p>
             <p>{this.props.gender}</p>
             <p>{this.props.job}</p>
-        </div> 
-        )
+            </div>
+        ) 
     }
 }
 
