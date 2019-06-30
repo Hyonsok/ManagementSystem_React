c21ac4e582e22984026a0551a6679f46cdd56b2e
Hyonsok <nahyonsok@gmail.com>
2019-06-30 12:17:15 -0700

    Apply Material-Ui and CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, {Component} from 'react';
 import Customer from './Components/Customer';
 import './App.css';
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+  width: '100%',
+  marginTop: theme.spacing.unit * 3,
+  overflowX: "auto"
+},
+  table: {
+    minWidth: 1080
+  }
+})
+
 
 // by adding [], customers are in an array
 const customers = [
@@ -30,27 +49,43 @@ const customers = [
 }
 ]
 
-// identified primary key, customer.id
+// identified primary key which is customer.id
 // iterate the customers by using map
 class App extends Component{
   render() {
+    const {classes} = this.props;
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (<Customer 
-              key = {c.id}
-              id = {c.id}
-              image = {c.image}
-              name = {c.name}
-              birthday = {c.birthday}
-              gender = {c.gender}
-              job = {c.job}
-            />);
-          })
-        }
-      </div>
+      <Paper className = {classes.root}>
+        <Table className = {classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Number</TableCell>
+                <TableCell>Image</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Birthday</TableCell>
+                <TableCell>Gender</TableCell>
+                <TableCell>Job</TableCell>
+              </TableRow>
+            </TableHead>
+            
+          <TableBody>
+          {
+            customers.map(c => {
+              return (<Customer 
+                key = {c.id}
+                id = {c.id}
+                image = {c.image}
+                name = {c.name}
+                birthday = {c.birthday}
+                gender = {c.gender}
+                job = {c.job}
+              />);
+            })
+          }
+          </TableBody> 
+        </Table>
+      </Paper>
       );
     }
   }
-export default App;
+export default withStyles(styles)(App);
