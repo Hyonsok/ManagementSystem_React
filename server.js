f951bd9822ca0e2e8c31e25d42cb34b5b78d8280
Hyonsok <nahyonsok@gmail.com>
2019-07-04 04:58:11 -0700

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req,res) => {
-    res.send([
-        {
-            'id' : 1,
-            'image' :  'https://placeimg.com/64/64/1', 
-            'name' : 'Hyonsok',
-            'birthday' : '20190629',
-            'gender' : 'Male',
-            'job' : 'student'
-        },
-        {
-            'id' : 2,
-            'image' :  'https://placeimg.com/64/64/2', 
-            'name' : 'Alex ',
-            'birthday' : '20190630',
-            'gender' : 'Male',
-            'job' : 'student'
-        }, 
-        {
-            'id' : 3,
-            'image' :  'https://placeimg.com/64/64/3',
-            'name' : 'Claire',
-            'birthday' : '201900619',
-            'gender' : 'Female',
-            'job' : 'student'
+    connection.query(
+        "SELECT * FROM customers",
+        (err,rows, fields) => {
+            res.send(rows);
         }
-    ]);
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
