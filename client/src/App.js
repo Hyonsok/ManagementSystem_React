8cac1865a0fed66e6dd75d126bd2613c1f08b1fa
Hyonsok <nahyonsok@gmail.com>
2019-07-07 11:08:39 -0700

    Applied App-Bar-Search-Field of material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -10,21 +10,89 @@ import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import CircularProgess from '@material-ui/core/CircularProgress';
 import {withStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
 
 // maintained the size 
 // created scroll bar if bigger
 const styles = theme => ({
   root: {
   width: '100%',
-  marginTop: theme.spacing.unit * 3,
-  overflowX: "auto"
-},
-table: {
   minWidth: 1080
-},
-progress: {
-  margin: theme.spacing.unit * 2
-}
+  },
+  menu: {
+    marginTop: 15,
+    marginBottom: 15,
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  paper: {
+    marginLeft: 18,
+    marginRight: 18
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
+  },
+  tableHaed: {
+    fontSize: '1.0rem'
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  }
 });
 
 /*
@@ -93,19 +161,47 @@ class App extends Component{
   }
   render() {
     const {classes} = this.props;
+    const cellList = ["ID","Image", "Name", "Birthday", "Gender", "Job"];
     return (
-      <div>
-        <Paper className = {classes.root}>
+      <div className = {classes.root}>
+        <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            Customer Management System
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'Search' }}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.menu}>
+      <CustomerAdd stateRefresh={this.stateRefresh}/>
+      </div>
+        <Paper className = {classes.paper}>
           <Table className = {classes.table}>
               <TableHead>
                 <TableRow>
-                  <TableCell>ID</TableCell>
-                  <TableCell>Image</TableCell>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Birthday</TableCell>
-                  <TableCell>Gender</TableCell>
-                  <TableCell>Job</TableCell>
-                  <TableCell>Option</TableCell>
+                  {cellList.map(c => {
+                    return <TableCell className = {classes.TableHead}>{c}</TableCell>
+                  })}
                 </TableRow>
               </TableHead>
               
@@ -132,7 +228,7 @@ class App extends Component{
             </TableBody> 
           </Table>
         </Paper>
-        <CustomerAdd stateRefresh={this.stateRefresh}/>
+        
       </div>
       );
     }
